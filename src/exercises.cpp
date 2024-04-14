93e71e91d964c483cd73d20fe8f2f2f37d22d606
Diegolew <159177910+Diegolew@users.noreply.github.com>
2024-04-14 16:12:04 -0600

    Intento 71

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -218,12 +218,15 @@ if (original == reversed) {
 }
 
 void exercise_15(int decimal) {
+if (decimal == 0) {
+  cout << "0";
+  return;
+}
 int binary[32];
 int i = 0;
-
 while (decimal > 0) {
   binary[i] = decimal % 2;
-  decimal /= 2; 
+  decimal /= 2;
   i++;
 }
 for (int j = i - 1; j >= 0; j--) {
