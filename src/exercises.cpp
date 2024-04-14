a4164dcc9827a1d2c6809f903545a68f08fb706e
Diegolew <159177910+Diegolew@users.noreply.github.com>
2024-04-14 16:01:34 -0600

    Intento 68

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -222,12 +222,12 @@ int binary[32];
 int i = 0;
 while (decimal > 0) {
   binary[i] = decimal % 2;
-  decimal /= 2;
+  decimal /= 10; 
   i++;
 }
 
 for (int j = i - 1; j >= 0; j--) {
-    cout << binary[j];
+  cout << binary[j];
   }
 }
 
